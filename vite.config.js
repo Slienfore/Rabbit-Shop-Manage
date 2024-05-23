0199dddfa185ac30f74155447922f645d80b99e6
Slienfore <Slienfore@163.com>
2024-05-23 17:33:05 +0800

    Element Plus 自动按需引入配置

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,23 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+// 配置 ElementPlus 按需导入
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // 按需引入
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     // 实际路径转化 @ -> src
     alias: {
