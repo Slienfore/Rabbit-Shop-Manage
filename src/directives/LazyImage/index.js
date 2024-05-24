a0d980067fea3c31abf1d07e1fca860b4271bb1b
Slienfore <Slienfore@163.com>
2024-05-24 15:54:31 +0800

    懒加载指令优化：加载完成之后，取消监听

--- src/directives/LazyImage/index.js
+++ src/directives/LazyImage/index.js
@@ -4,11 +4,13 @@ export default {
   // el: 指令绑定的元素 -> img
   // binding: binding.value 指令等于号后面绑定的表达式的值 -> img.src
   mounted(el, binding) {
-    useIntersectionObserver(el, ([{ isIntersecting }]) => {
+    const { stop } = useIntersectionObserver(el, ([{ isIntersecting }]) => {
       if (!isIntersecting) return;
 
       // 进入视口区域 -> 绑定 src
       el.src = binding.value;
+
+      stop(); // 第一次加载完图片, 取消监听
     });
   },
 };
